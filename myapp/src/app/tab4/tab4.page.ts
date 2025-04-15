128c87e404558f3b1c2f15fc9bfb989f1f3547aa
guziqian03 <z.gu.13@student.scu.edu.au>
2025-04-15 13:29:00 +0800

    Creat new tab

---
+++ myapp/src/app/tab4/tab4.page.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCardTitle, IonCardHeader, IonCard, IonCardSubtitle, IonCardContent, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { HelpsComponent } from '../helps/helps.component';
+import { addIcons } from 'ionicons';
+import { 
+  lockClosed, 
+  shieldCheckmark, 
+  documentText, 
+  mail,
+  helpCircle 
+} from 'ionicons/icons';
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: 'tab4.page.html',
+  styleUrls: ['tab4.page.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    HelpsComponent,
+    IonButton, 
+    IonIcon, 
+    IonCardContent, 
+    IonCardSubtitle, 
+    IonCard, 
+    IonCardHeader, 
+    IonCardTitle, 
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    ExploreContainerComponent
+  ],
+})
+export class Tab4Page {
+  constructor() {
+    addIcons({
+      lockClosed,
+      shieldCheckmark,
+      documentText,
+      mail,
+      helpCircle
+    });
+  }
+}
